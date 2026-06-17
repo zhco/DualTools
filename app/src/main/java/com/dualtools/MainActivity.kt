28daa4b3139d936a7481408d108cfe128eb1cce8
zhco <luycheung@qq.com>
2026-06-17 12:21:18 +0800

    v68: crash fix

--- app/src/main/java/com/dualtools/MainActivity.kt
+++ app/src/main/java/com/dualtools/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dualtools
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,10 +10,26 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.dualtools.ui.screens.HomeScreen
 import com.dualtools.ui.theme.DualToolsTheme
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Global crash handler - captures ALL uncaught exceptions including native crashes
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            writeCrashLog(throwable)
+            defaultHandler?.uncaughtException(thread, throwable)
+            // Force kill after native crash to avoid corrupted state
+            android.os.Process.killProcess(android.os.Process.myPid())
+        }
+
         setContent {
             DualToolsTheme {
                 Surface(
@@ -23,5 +40,21 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun writeCrashLog(throwable: Throwable) {
+        try {
+            val crashDir = File(getExternalFilesDir(null), "crashes")
+            crashDir.mkdirs()
+            val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+            val crashFile = File(crashDir, "crash_$timestamp.txt")
+
+            val sw = StringWriter()
+            val pw = PrintWriter(sw)
+            throwable.printStackTrace(pw)
+            pw.flush()
+
+            crashFile.writeText("Time: $timestamp\nThread: ${Thread.currentThread().name}\n\n$sw")
+        } catch (_: Throwable) {}
     }
 }
